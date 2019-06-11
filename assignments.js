78a7ea48aa791f01daab323471b64b9ac0c75e4b
Nelly31 <nelly_main@hotmail.com>
2019-06-11 02:06:25 +0000

    Calculate the area of shapes

--- assignments.js
+++ assignments.js
@@ -6,9 +6,46 @@
 
 // console.log(ageCalculator("Nelly", 1987, 2019));
 
-const howManyHundreds = function(syrup) {
-  return (syrup - (syrup % 100)) /100
+// const howManyHundreds = function(syrup) {
+//   return (syrup - (syrup % 100)) /100
+// }
+
+// console.log(howManyHundreds(894));
+
+const calculateRectangleArea = function(length , width){
+  if (length < 0 || width < 0){
+    return undefined
+  }
+  return length * width 
 }
 
-console.log(howManyHundreds(894));
+console.log(calculateRectangleArea(10,5));
 
+console.log(calculateRectangleArea(1.5,2.5));
+
+console.log(calculateRectangleArea(-1,5))
+
+
+const calculateTriangleArea = function(base,height){
+  if (base < 0 || height <0){
+    return undefined
+  }
+  return (base * height)/2
+}
+
+console.log(calculateTriangleArea(10,5))
+
+console.log(calculateTriangleArea(3,2.5))
+
+console.log(calculateTriangleArea(-1,5))
+
+const calculateCircleArea = function(radius){
+  if (radius < 0) {
+    return undefined
+  }
+  return Math.PI * Math.pow(radius,2)
+}
+
+console.log(calculateCircleArea(10));
+
+console.log(calculateCircleArea(3.5))
