bc1678a7e077668c518c3a4c663f9f270f37f182
Nelly31 <nelly_main@hotmail.com>
2019-06-11 19:28:29 +0000

    lastIndex function

--- assignments.js
+++ assignments.js
@@ -63,17 +63,28 @@
 // console.log(iceCreamFlavours.length);
 
 
-let range = function(start,end,step){
-  let numArray = []
-  if (start === undefined || end === undefined || step === undefined || start > end || step <=0){
-    return numArray
-  }
-  for(i=start; i<=end; i+=step){
-    numArray.push(i) 
-  }
-  return numArray
+// let range = function(start,end,step){
+//   let numArray = []
+//   if (start === undefined || end === undefined || step === undefined || start > end || step <=0){
+//     return numArray
+//   }
+//   for(i=start; i<=end; i+=step){
+//     numArray.push(i) 
+//   }
+//   return numArray
+// }
+
+// console.log(range(0, 10, 2));
+// console.log(range(10, 30, 5));
+// console.log(range(-5, 2, 3));
+
+const lastIndexOf = function(myArray,value){
+  for (let i = myArray.length-1; i >= 0; i--){
+    if (myArray[i] === value) {
+      return i;
+    } 
+  } 
+  return -1;
 }
 
-console.log(range(0, 10, 2));
-console.log(range(10, 30, 5));
-console.log(range(-5, 2, 3));
+console.log(lastIndexOf([0,1,4,1,2],1))
